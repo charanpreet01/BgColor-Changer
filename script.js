3de30b893456e0b8f84b54c9af6fef81395c078c
charanpreet01 <charanpreetsinghrajput@gmail.com>
2024-04-12 22:00:05 +0530

    code updated

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ const stop = document.querySelector("#stop");
 const body = document.querySelector("body");
 const h2 = document.querySelector("h2");
 
+let colorCode = "";
+
 const randomColor = () => {
     const hex = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)]
     }
+    console.log(color);
+    colorCode = color;
+
     return color;
 }
 
@@ -17,6 +22,8 @@ let changeMe;
 start.addEventListener("click", () => {
     changeMe = setInterval(() => {
         body.style.backgroundColor = randomColor();
+        h2.innerHTML = colorCode;
+        h2.style.display="block";
     }, 1000);
     start.setAttribute("disabled", "");
 })
